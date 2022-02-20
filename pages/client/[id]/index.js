81596cf7563e90387f22a9cb5ad6720ebf50c906
wink-aajlak <aajlak.wink@gmail.com>
2022-02-20 08:50:51 +0530

    dynamic folder and file routes

---
+++ pages/client/[id]/index.js
@@ -0,0 +1,11 @@
+const ClientProjectsPage = () => {
+  return (
+    <div>
+      <h1>The Projects of a given Client</h1>
+    </div>
+  );
+}
+
+export default ClientProjectsPage;
+
+// http://localhost:3000/client/someclient
---
+++ pages/client/index.js
@@ -0,0 +1,11 @@
+const ClientPage = () => {
+  return (
+    <div>
+      <h1>The Client Page</h1>
+    </div>
+  );
+}
+
+export default ClientPage;
+
+// http://localhost:3000/client
